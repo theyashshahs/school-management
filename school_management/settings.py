32d421f7dfa4c993d9224790cc70e9a2b88b698e
yashshah2820 <201851150@iiitvadodara.ac.in>
2020-05-31 23:52:22 +0530

    staticfiles root added

--- school_management/settings.py
+++ school_management/settings.py
@@ -26,7 +26,6 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-# ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
