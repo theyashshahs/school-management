0144b9676dac2560141ecc085b8066ddc6201377
yashshah2820 <201851150@iiitvadodara.ac.in>
2020-05-31 23:33:53 +0530

    heroku deployment

--- school_management/urls.py
+++ school_management/urls.py
@@ -3,7 +3,6 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-
 from . import views
 
 app_name = 'home'
--- student/views.py
+++ student/views.py
@@ -7,7 +7,3 @@ from student.models import Student
 class StudentViewSet(ModelViewSet):
     serializer_class = StudentSerializer
     queryset = Student.objects.all()
-
-
-# def index(request):
-#     return render(request, 'student/student.html')
