fdc0123f06f4b19cfdf256e3077d5a1f803af11a
yashshah2820 <201851150@iiitvadodara.ac.in>
2020-05-31 23:56:18 +0530

    ALLOWED_HOSTS :bug:

--- school_management/settings.py
+++ school_management/settings.py
@@ -26,8 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost']
 
 # Application definition
 
