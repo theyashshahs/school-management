1bba2a4c3cbd9afbbffb390887e3a31f2a07c72e
yashshah2820 <201851150@iiitvadodara.ac.in>
2020-06-01 00:20:03 +0530

    django-heroku settings added :heavy_check_mark:

--- school_management/settings.py
+++ school_management/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 import os
 from decouple import config
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -136,3 +137,5 @@ STATICFILES_DIRS = [
 # for media under development
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+django_heroku.settings(locals())
