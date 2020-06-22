d8c0a23f91957a1450369c3d00773569189c5a8c
ashutoshsingh05 <201851029@iiitvadodara.ac.in>
2020-06-22 23:45:43 +0530

    Enable CORS
    Without Cross origin resource sharing, we won't be able to access the API from a different domain

--- school_management/settings.py
+++ school_management/settings.py
@@ -53,9 +53,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'school_management.urls'
+CORS_ORIGIN_ALLOW_ALL = True
 
 TEMPLATES = [
     {
