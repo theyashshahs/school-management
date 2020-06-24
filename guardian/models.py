d55979ef30b61b250210e4ab479afa65da0ad28b
yashshah2820 <201851150@iiitvadodara.ac.in>
2020-06-24 21:45:47 +0530

    bug fixed :bug:

--- guardian/models.py
+++ guardian/models.py
@@ -2,6 +2,6 @@ from django.db import models
 from student.models import Student
 
 class Guardian(models.Model):
-    student = models.ForeignKey('Student', on_delete=models.CASCADE)
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
     
 
