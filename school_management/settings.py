ca22c37e3cd356b74a2c77406846db2d646d1ddc
yashshah2820 <201851150@iiitvadodara.ac.in>
2020-06-26 18:24:13 +0530

    cors :bug: removed

--- school_management/settings.py
+++ school_management/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -56,7 +57,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'school_management.urls'
@@ -113,7 +113,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 
 # cors headers
-CORS_ORIGIN_ALLOW_ALL = True
+CORS_ORIGIN_ALLOW_ALL=True
 
 CORS_ALLOW_HEADERS = [
     'accept',
