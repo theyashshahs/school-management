1ce28f036d096b0ff6348483af624d5be78a11ac
yashshah2820 <201851150@iiitvadodara.ac.in>
2020-06-26 18:10:14 +0530

    cors headers added

--- school_management/settings.py
+++ school_management/settings.py
@@ -115,6 +115,27 @@ AUTH_PASSWORD_VALIDATORS = [
 # cors headers
 CORS_ORIGIN_ALLOW_ALL = True
 
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+]
+
+CORS_ALLOW_METHODS = [
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+]
+
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
