555a16af86fea28ce7a1f37ea64770eead31ac95
yashshah2820 <201851150@iiitvadodara.ac.in>
2020-06-26 18:33:17 +0530

    str function added to guardian model

--- guardian/models.py
+++ guardian/models.py
@@ -5,3 +5,5 @@ class Guardian(models.Model):
     student = models.ForeignKey(Student, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     
+    def __str__(self):
+        return f'{self.name} {self.student}'
