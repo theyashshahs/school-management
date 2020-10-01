64ebcd3a23e7a4476ecbfa805d143013eca04d87
Restyled.io <commits@restyled.io>
2020-10-01 05:25:15 +0000

    Restyled by isort

--- school_management/urls.py
+++ school_management/urls.py
@@ -1,5 +1,5 @@
-from django.conf.urls.static import static
 from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import include, path
--- teacher/tests.py
+++ teacher/tests.py
@@ -1,6 +1,6 @@
+from django.contrib.auth.models import User
 from django.http import response
 from django.test import TestCase
-from django.contrib.auth.models import User
 from rest_framework import status
 from rest_framework.test import APIRequestFactory, force_authenticate
 
