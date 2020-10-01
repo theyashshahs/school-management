921fd0380addfe9afdf345b9f064a07af64f4387
Yash Shah <201851150@iiitvadodara.ac.in>
2020-10-01 10:40:00 +0530

    fixed bugs

--- school_management/urls.py
+++ school_management/urls.py
@@ -1,8 +1,9 @@
-from django.contrib import admin
-from django.urls import path, include
-from django.conf import settings
 from django.conf.urls.static import static
+from django.conf import settings
+from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.urls import include, path
+
 from . import views
 
 app_name = 'home'
@@ -20,4 +21,3 @@ urlpatterns = [
 
 urlpatterns += staticfiles_urlpatterns()
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
