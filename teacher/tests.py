553148a5f1a628e1187e2b9eeeea79bce5210228
Yash Shah <201851150@iiitvadodara.ac.in>
2020-10-01 10:54:02 +0530

    added API test for the teacher and gaurdian models

--- teacher/tests.py
+++ teacher/tests.py
@@ -1,3 +1,17 @@
+from django.http import response
 from django.test import TestCase
+from django.contrib.auth.models import User
+from rest_framework import status
+from rest_framework.test import APIRequestFactory, force_authenticate
 
-# Create your tests here.
+from teacher.models import Teacher
+
+
+class TeacherTests(TestCase):
+    def setUp(self):
+        self.teacher = Teacher.objects.create(
+            teacher_id=1, first_name='optimus', last_name='prime')
+
+    def test_get_all_guardian(self):
+        response = self.client.get('/teachers/teacher/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
